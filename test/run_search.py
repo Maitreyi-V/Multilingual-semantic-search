109a60d0f0bff0ae451542ce90653db3269ffb9f
Maitreyi V <vijayananda@Maitreyis-MacBook-Pro.local>
2025-06-07 08:50:20 +0530

    Add run_search.py

--- test/run_search.py
+++ test/run_search.py
@@ -1,7 +1,7 @@
 from processor import search_phrase
 
 def main():
-   
+    
     with open("sample.txt", "r") as f:
         content = f.read()
 
@@ -13,11 +13,5 @@ def main():
     else:
         print("Phrase not found!")
 
-def search(query, file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    return [line.strip() for line in lines if query.lower().strip() in line.lower()]
-
-y
 if __name__ == "__main__":
     main()
